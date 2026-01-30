72edd58bd0ac0b12a849de5aa78b82fb68061827
Brijesh Yadav <brijeshyadavcs12@gmail.com>
2026-01-30 21:51:54 +0530

    Email Controller has added

---
+++ src/main/java/com/Assistant/email/Controller/EmailGeneratorController.java
@@ -0,0 +1,32 @@
+package com.Assistant.email.Controller;
+
+
+import com.Assistant.email.Service.EmailGeneratorService;
+import com.Assistant.email.DTO.RequestEmail;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/email")
+@AllArgsConstructor
+@Data
+public class EmailGeneratorController {
+   private final EmailGeneratorService emailGeneratorService;
+
+   @PostMapping("/generate")
+   public ResponseEntity<String> generateEmail(@RequestBody RequestEmail requestEmail){
+       String response=emailGeneratorService.generateEmailReply(requestEmail);
+       return ResponseEntity.ok(response);
+   }
+
+
+
+
+
+
+}
