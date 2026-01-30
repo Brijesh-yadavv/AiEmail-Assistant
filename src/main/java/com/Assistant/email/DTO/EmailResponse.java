8c6c749174a9eafa5c9b650f6e1492a7c3f70d82
Brijesh Yadav <brijeshyadavcs12@gmail.com>
2026-01-30 21:59:33 +0530

    API working for response to email

---
+++ src/main/java/com/Assistant/email/DTO/EmailResponse.java
@@ -0,0 +1,10 @@
+package com.Assistant.email.DTO;
+
+import lombok.Data;
+
+@Data
+public class EmailResponse{
+private String reply;
+
+
+}
---
+++ src/main/java/com/Assistant/email/DTO/RequestEmail.java
@@ -0,0 +1,13 @@
+package com.Assistant.email.DTO;
+
+
+import lombok.Data;
+
+@Data
+public class RequestEmail {
+
+    private String emailContent;
+    private String tone;
+
+
+}
