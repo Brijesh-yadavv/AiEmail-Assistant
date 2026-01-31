ca071fbd673c2a61c20c125f95007ba3e1dbf836
Brijesh Yadav <brijeshyadavcs12@gmail.com>
2026-01-31 15:50:47 +0530

    This commit is all about Working with webclient, RestTemplate has been removed and WevClient has been added

--- src/main/java/com/Assistant/email/Controller/EmailGeneratorController.java
+++ src/main/java/com/Assistant/email/Controller/EmailGeneratorController.java
@@ -5,6 +5,7 @@ import com.Assistant.email.Service.EmailGeneratorService;
 import com.Assistant.email.DTO.RequestEmail;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,10 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/email")
-@AllArgsConstructor
+
 @Data
 public class EmailGeneratorController {
    private final EmailGeneratorService emailGeneratorService;
+
+
+
 
    @PostMapping("/generate")
    public ResponseEntity<String> generateEmail(@RequestBody RequestEmail requestEmail){
